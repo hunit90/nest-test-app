4b62b1aafebeaee7b691452abfc1b22f27335632
hunit <hunit612@naver.com>
2024-02-17 23:58:24 +0900

    exception filter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Ip } from './decorators/ip.decorator';
+import { HttpExceptionFilter } from './exceptions/http.exceptions';
 
 @Controller()
 export class AppController {
@@ -11,6 +12,7 @@ export class AppController {
     @Ip() ip: string
   ): string {
     console.log(ip)
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    throw new HttpException('NotFound', HttpStatus.NOT_FOUND)
   }
 }
