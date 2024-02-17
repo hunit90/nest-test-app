1bf952ced58db7ba3ab366930603397725f15049
hunit <hunit612@naver.com>
2024-02-18 00:11:32 +0900

    logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Ip } from './decorators/ip.decorator';
 import { HttpExceptionFilter } from './exceptions/http.exceptions';
@@ -7,11 +7,17 @@ import { HttpExceptionFilter } from './exceptions/http.exceptions';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  private readonly logger = new Logger(AppController.name)
+
   @Get()
   getHello(
     @Ip() ip: string
   ): string {
     console.log(ip)
+    this.logger.log(ip)
+    this.logger.debug(ip)
+    this.logger.error(ip)
+    this.logger.warn(ip)
     // return this.appService.getHello();
     throw new HttpException('NotFound', HttpStatus.NOT_FOUND)
   }
