72e1f5610cae85954556a1225ff239a539382c4c
hunit <hunit612@naver.com>
2024-03-01 00:00:01 +0900

    build

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 
-config({ path: '.env' });
+config({ path: `.env.${process.env.NODE_ENV}` });
 
 export default new DataSource({
   type: 'postgres',
